f38573fd909fa5f3d62a31e256a4464b5dc0e8fc
shenghaowang <wangshenghao1993@gmail.com>
2024-11-11 15:21:22 +0000

    Add relu activation function

---
+++ src/neural_network/activation/test_relu.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+
+from neural_network.activation.relu import ReLU
+
+
+@pytest.fixture
+def relu() -> ReLU:
+    return ReLU()
+
+
+@pytest.fixture
+def inputs() -> np.ndarray:
+    inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+    return np.array(inputs)
+
+
+def test_forward(relu: ReLU, inputs: np.ndarray):
+    relu.forward(inputs)
+    assert np.array_equal(relu.output, np.array([0, 2, 0, 3.3, 0, 1.1, 2.2, 0]))
