2c0d222c86065a90a6ac62049aaabb094447bbe5
root <wangshenghao1993@gmail.com>
2026-04-25 16:57:34 +0000

    Solution without using numpy

--- linear_algebra/deepml_mat_eigenvalues.py
+++ linear_algebra/deepml_mat_eigenvalues.py
@@ -2,8 +2,18 @@ import numpy as np
 
 
 def calculate_eigenvalues(matrix: list[list[float | int]]) -> list[float]:
-    eigenvalues, _ = np.linalg.eig(matrix)
-    return eigenvalues
+    # Solve (a - lambda)(d - lambda) - bc = 0
+    # lambda^2 - (a+d)lambda + (ad-bc) = 0
+    a, b, c, d = matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
+    trace = a + d
+    determinant = a * d - b * c
+    discriminant = trace**2 - 4 * determinant
+
+    # Solve for lambdas
+    lambda_1 = (trace + discriminant**0.5) / 2
+    lambda_2 = (trace - discriminant**0.5) / 2
+
+    return [lambda_1, lambda_2]
 
 
 def test_calculate_eigenvalues(
